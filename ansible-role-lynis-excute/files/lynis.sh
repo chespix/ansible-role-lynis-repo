3cffb746b5c16f685b2afd3b9e7f3f210cc58bfd
Rethesh Nair <nair@pythian.com>
2016-06-25 17:11:53 +0000

    updated the ansible-role-lynis-excute/files/lynis.sh for proper execution and ansible-role-lynis-excute/tasks/main.yml for fetching the output

--- ansible-role-lynis-excute/files/lynis.sh
+++ ansible-role-lynis-excute/files/lynis.sh
@@ -1,4 +1,28 @@
-#!/bin/bash
-log_dir=/var/log/lynis
-dest_dir=/opt/lynis
-cd "$dest_dir" && sudo ./lynis -c --auditor "Job" --cronjob -Q > $log_dir/report-$(hostname).$(date +%Y%m%d).txt && mv /var/log/lynis.log $log_dir/report-log-$(hostname).$(date +%Y%m%d).log && mv /var/log/lynis-report.dat $log_dir/report-data-$(hostname).$(date +%Y%m%d).txt
+#!/bin/sh
+
+AUDITOR="automated"
+DATE=$(date +%Y%m%d)
+HOST=$(hostname)
+LOG_DIR="/var/log/lynis"
+REPORT="$LOG_DIR/report-${HOST}.${DATE}"
+DATA="$LOG_DIR/report-data-${HOST}.${DATE}"
+LOG="$LOG_DIR/report-log-${HOST}.${DATE}"
+DEST=/opt/lynis
+
+rm -rf "$LOG_DIR/*"
+
+cd "${DEST}"
+
+
+# Run Lynis
+./lynis audit system --auditor "${AUDITOR}" --cronjob > ${REPORT}
+
+# Optional step: Move report file if it exists
+if [ -f /var/log/lynis-report.dat ]; then
+    mv /var/log/lynis-report.dat ${DATA}
+fi
+if [ -f /var/log/lynis.log ]; then
+    mv /var/log/lynis.log ${LOG}
+fi
+
+# The End
